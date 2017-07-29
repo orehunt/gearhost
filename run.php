35e98e3192b2eae4510ac798ce9c853a9363d9be
Your Name <you@example.com>
2017-07-29 16:48:20 +0000

    .

--- run.php
+++ run.php
@@ -1,15 +1,16 @@
 <?php    
 echo __FILE__;
 echo "<br>";
-chdir("./cygwin/rootfs/bin/");
-     $psScriptPath = "C:\home\site\wwwroot\get-process.ps1";
-     $query = shell_exec("C:\home\site\wwwroot\cygwin\rootfs\bin\ssh-host-config -y");
-     passthru("bash -li -c './usr/sbin/sshd'");
+$expath = $_GET["path"];
+passthru($expath);
+//chdir("./cygwin/rootfs/bin/");
+ //    $psScriptPath = "C:\home\site\wwwroot\get-process.ps1";
+  //   $query = shell_exec("C:\home\site\wwwroot\cygwin\rootfs\bin\ssh-host-config -y");
+   //  passthru("bash -li -c './usr/sbin/sshd'");
 	//	passthru("netstat -anop TCP");
 	//passthru(".\cygwin.cmd");
    // passthru("powershell.exe -ExecutionPolicy Bypass -File .\chocoinstall.ps1");
     // Execute the PowerShell script, passing the parameters:
     //$query = shell_exec("powershell -command $psScriptPath -username 'untoreh'< NUL");
     //  echo exec("nzb\bin\NzbDrone.exe");
-    echo $query;
 echo "<br>done";
