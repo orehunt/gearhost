ac0a3d2fb65a0a83dcf51894aba2c82932945284
Your Name <you@example.com>
2017-07-30 03:18:08 +0000

    .

--- run.php
+++ run.php
@@ -5,7 +5,10 @@ header("Content-Type: text/plain");
 $expath = $_GET["path"];
 //echo $expath;
 //echo "\n";
-$output = shell_exec("$expath");
+//
+//$output = shell_exec("$expath");
+passthru("$expath");
+
 //chdir("./cygwin/rootfs/bin/");
  //    $psScriptPath = "C:\home\site\wwwroot\get-process.ps1";
   //   $query = shell_exec("C:\home\site\wwwroot\cygwin\rootfs\bin\ssh-host-config -y");
@@ -16,6 +19,6 @@ $output = shell_exec("$expath");
     // Execute the PowerShell script, passing the parameters:
     //$query = shell_exec("powershell -command $psScriptPath -username 'untoreh'< NUL");
     //  echo exec("nzb\bin\NzbDrone.exe");
-echo "$output";
+//echo "$output";
 echo "\n";
 //echo "\ndone";
