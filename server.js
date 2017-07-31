9cc08302b2e5992fcfbac918d66c0324a6cdf069
Your Name <you@example.com>
2017-07-31 10:42:25 +0000

    .

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-ssh2/run.js
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('id: ' + req.query.id);
+});
+
+app.use(express.static('.'));
+
+app.listen(80, '0.0.0.0', function() {
+	console.log('Listening on port ' + this.address().port);
+});
+
