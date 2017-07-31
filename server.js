ac6505885cbbfaf69e923a5a06c083c1cdecd0bc
Your Name <you@example.com>
2017-07-31 15:02:55 +0000

    .

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
-//var express = require('express');
-//var app = express();
-//var port = process.env.PORT || '3000';
-// app.use(express.static('.'));
-////app.get('/', function(req, res){
-//  
-//  res.send('id: ' + req.query.id);
-//});
-//app.listen(port, function() {
-//	console.log('Listening on port ' + this.address().port);
-//});
-
-var http = require('http');
+var express = require('express');
+var app = express();
 var port = process.env.PORT || '3000';
-
-http.createServer(function (req, res) {
-       // res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Hello World\n');
-}).listen(port);
+app.use(express.static('.'));
+app.get('/', function(req, res){
+	res.send('id: ' + req.query.id);
+});
+app.listen(port, function() {
+	console.log('Listening on port ' + this.address().port);
+});
