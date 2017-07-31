15bf415d185243957ede663f34a1e7e87ae83aff
Your Name <you@example.com>
2017-07-31 10:58:46 +0000

    .

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('.'));
 app.get('/', function(req, res){
   res.send('id: ' + req.query.id);
 });
-
-app.use(express.static('.'));
-
 app.listen(80, '0.0.0.0', function() {
 	console.log('Listening on port ' + this.address().port);
 });
-
