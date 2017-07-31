8dbec951f4f3659acaba56ae1589a530bc7952ea
Your Name <you@example.com>
2017-07-31 14:33:45 +0000

    .

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-ssh2/run.js
+var http = require('http');
+var port = process.env.PORT || '3000';
+
+http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'text/plain'});
+res.end('Hello World\n');
+}).listen(port);
