af7568cbcbc7b03c17160fb6f2c108d66eec5229
Your Name <you@example.com>
2017-08-07 17:06:35 +0000

    .

---
+++ cygwin.cmd
@@ -0,0 +1,12 @@
+setup-x86.exe ^
+--no-admin ^
+--no-desktop ^
+--no-startmenu ^
+--root .\cygwin ^
+--site http://cygwin.mirror.constant.com ^
+--quiet-mode ^
+--packages ^
+busybox^
+openssl^
+openssh^
+gzip^
