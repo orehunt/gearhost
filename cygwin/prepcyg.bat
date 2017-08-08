ddfe1e4170c1717a26864440c5cb06111d299c44
Your Name <you@example.com>
2017-08-08 05:56:50 +0000

    .

---
+++ cygwin/prepcyg.bat
@@ -0,0 +1,17 @@
+@echo off
+
+C:
+setup-x86.exe ^
+--no-admin ^
+--no-desktop ^
+--no-startmenu ^
+--root .\cygwin ^
+--site http://cygwin.mirror.constant.com ^
+--quiet-mode ^
+--packages busybox,binutils,openssh,wget
+
+copy openssh-7.4p1-1.tar.xz .\cygwin
+copy apt-cyg .\cygwin\bin
+chdir .\cygwin\bin
+bash --login -i -c 'tar xf /openssh-7.4p1-1.tar.xz -C /'
+bash --login -i -c '/bin/echo.exe -e "yes\nyes\nno\nno\nno\n" | bash /usr/bin/ssh-host-config'
