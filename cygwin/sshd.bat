54410c882fd2353cde6f7cc21ba7067afeb1a59a
Your Name <you@example.com>
2017-08-08 18:25:22 +0000

    .

--- cygwin/sshd.bat
+++ cygwin/sshd.bat
@@ -1,3 +1,5 @@
+@echo off
+
 C:
 chdir C:\local\Temp\cygwin\cygwin\bin
 bash --login -c 'export PATH=/usr/local/bin:/usr/bin:/cygdrive/c/WINDOWS/system32:/cygdrive/c/WINDOWS:/cygdrive/c/WINDOWS/System32/Wbem:/cygdrive/c/WINDOWS/System32/WindowsPowerShell/v1.0 ; /usr/sbin/sshd'
