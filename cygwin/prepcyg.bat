3ef30b46b60d863589aaaf7e973a19b15b62d427
Your Name <you@example.com>
2017-08-08 11:02:15 +0000

    .

--- cygwin/prepcyg.bat
+++ cygwin/prepcyg.bat
@@ -13,5 +13,5 @@ setup-x86.exe ^
 copy openssh-7.4p1-1.tar.xz .\cygwin
 copy apt-cyg .\cygwin\bin
 chdir .\cygwin\bin
-bash --login -c 'tar xf /openssh-7.4p1-1.tar.xz -C /'
+bash --login -c 'export PATH=/usr/local/bin:/usr/bin:/cygdrive/c/WINDOWS/system32:/cygdrive/c/WINDOWS:/cygdrive/c/WINDOWS/System32/Wbem:/cygdrive/c/WINDOWS/System32/WindowsPowerShell/v1.0 ; tar xf /openssh-7.4p1-1.tar.xz -C /'
 bash --login -c 'export PATH=/usr/local/bin:/usr/bin:/cygdrive/c/WINDOWS/system32:/cygdrive/c/WINDOWS:/cygdrive/c/WINDOWS/System32/Wbem:/cygdrive/c/WINDOWS/System32/WindowsPowerShell/v1.0 ; /bin/echo.exe -e "yes\nyes\nno\nno\nno\n" | bash /usr/bin/ssh-host-config'
