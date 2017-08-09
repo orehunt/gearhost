67338e732c23dcba38c3237f6ae5f764725fc8d0
Your Name <you@example.com>
2017-08-09 04:37:27 +0000

    .

--- cygwin/sshd.bat
+++ cygwin/sshd.bat
@@ -2,4 +2,4 @@
 
 C:
 chdir C:\local\Temp\cygwin\cygwin\bin
-bash --login -c 'export PATH=/usr/local/bin:/usr/bin:/cygdrive/c/WINDOWS/system32:/cygdrive/c/WINDOWS:/cygdrive/c/WINDOWS/System32/Wbem:/cygdrive/c/WINDOWS/System32/WindowsPowerShell/v1.0 ; /usr/sbin/sshd'
+bash --login -c 'export PATH=/usr/local/bin:/usr/bin:/cygdrive/c/WINDOWS/system32:/cygdrive/c/WINDOWS:/cygdrive/c/WINDOWS/System32/Wbem:/cygdrive/c/WINDOWS/System32/WindowsPowerShell/v1.0 ; /usr/sbin/sshd -E C:\local\Temp\cygwin\sshd.log -h /cygdrive/c/home/site/wwwroot/cygwin/ssh_host_rsa_key'
